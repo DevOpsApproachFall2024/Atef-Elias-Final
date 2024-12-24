e20bcc5b73fc8c808ede744f95748da3b38394c9
atef fawaz <atefawaz@gmail.com>
2024-12-24 22:05:14 +0100

    Fix RuboCop offenses

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,3 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
-
