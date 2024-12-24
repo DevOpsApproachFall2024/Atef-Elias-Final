53809e5f5672a868d8fa29caa3452dda2988d682
atef fawaz <137223907+atefawaz@users.noreply.github.com>
2024-12-24 22:42:12 +0100

    Feature/user auth (#3)
    * User-auth
    
    * Fix linting issues
    
    * rubylinting
    
    * Update and rename main.yml to Ruby-linting.yml
    
    * Feature/user auth (#1)
    
    * User-auth
    
    * Fix linting issues
    
    * rubylinting
    
    * fix workflows
    
    * fixing linting issues
    
    * Fix RuboCop offenses
    
    * Fix YAML syntax for Ruby Linting workflow

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
-    def home
-      render json: { message: "Welcome to the TODO App API" }
-    end
+  SECRET_KEY = "f2bf023ae6738c19a0e686185d6a85be53afb21aab072d25f3218fbdb69bf94b0fc3f0939f25bed6e864b268929cac33a455489970293040ae479c069763bdd1"
+
+  def decode_token(token)
+    JWT.decode(token, SECRET_KEY)[0] rescue nil
   end
-  
+end
