13cdb8e29d9993b1c93cd4f6332619ab962aed56
atef fawaz <137223907+atefawaz@users.noreply.github.com>
2024-12-24 21:52:59 +0100

    Feature/user auth (#1)
    * User-auth
    
    * Fix linting issues
    
    * rubylinting

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :tasks, dependent: :destroy
-    validates :email, presence: true, uniqueness: true
+
     validates :username, presence: true, uniqueness: true
-  end
-  
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
+
