5cb0672c26edd4c20cdf1e89e87ac26370d2b038
atef fawaz <atefawaz@gmail.com>
2024-12-24 23:43:47 +0100

    fixing linting errors - task management implemented

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+
   validates :title, presence: true
   validates :status, inclusion: { in: %w[Pending InProgress Completed] }
 end
--- backend/db/migrate/20241224195601_create_tasks.rb
+++ backend/db/migrate/20241224195601_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[8.0]
     create_table :tasks do |t|
       t.string :title
       t.text :description
-      t.string :status
+      t.string :status, default: 'Pending'
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
